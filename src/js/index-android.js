bdd6ea153e6bd74bd674459e691b0bc0b05e0ef5
Maximilian Meier <maxi.meier92@gmail.com>
2020-08-14 10:30:40 +0200

    Update index-android.js

--- src/js/index-android.js
+++ src/js/index-android.js
@@ -112,7 +112,7 @@ inAppPurchase.restorePurchases = () => {
             productId: val.productId,
             state : val.state || val.purchaseState,
             transactionId: val.orderId,
-            date : val.date || val.purchaseDate,
+            date : val.date || val.purchaseTime,
             type : val.type,
             productType : val.type,
             signature: val.signature,
